018c1e32dbd2442b8a49c90f91be27d7eeddeb3f
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2024-09-29 18:16:39 -0500

    Moss giant movement

--- Assets/Code/Scripts/Enemy/MossGiant.cs
+++ Assets/Code/Scripts/Enemy/MossGiant.cs
@@ -2,8 +2,27 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
+    private Vector3 currentTarget;
+    private void Start()
+    {
+        //
+    }
     public override void Update()
     {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+        }
+        else if (transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+        }
         
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
     }
 }
